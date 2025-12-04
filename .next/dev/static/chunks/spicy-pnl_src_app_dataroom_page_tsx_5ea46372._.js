5a0fa53deedbf2f1950ac6796a2384d651b82326
bitwiseberto <rpn241@nyu.edu>
2025-12-03 22:42:42 -0500

    moneybag

---
+++ .next/dev/static/chunks/spicy-pnl_src_app_dataroom_page_tsx_5ea46372._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/spicy-pnl_src_5d5b76d1._.js",
+  "static/chunks/0457b_next_970e58bf._.js"
+],
+    source: "dynamic"
+});
